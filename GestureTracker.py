b50ca206e79e9723a9f15919595689d3b70272c1
Ryan Zoeller <rtzoeller@rtzoeller.com>
2016-11-27 16:53:13 -0600

    Typo

--- GestureTracker.py
+++ GestureTracker.py
@@ -2,7 +2,7 @@ from __future__ import division, print_function
 
 import numpy as np
 
-from IntepolateTracker import InterpolateTracker
+from InterpolateTracker import InterpolateTracker
 
 
 def main(points):
