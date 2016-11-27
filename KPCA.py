663a4d65a41dd3e83f6021bf0f95a01bf0ebfa79
Ryan Zoeller <rtzoeller@rtzoeller.com>
2016-11-27 15:49:48 -0600

    Refactor prediction logic
    Pull prediction logic from main method into separate class, to
    facilitate integration with ROS

--- KPCA.py
+++ KPCA.py
@@ -13,8 +13,9 @@ from math import exp
 
 
 class KPCA(object):
-    def __init__(self, trajectories, gamma):
+    def __init__(self, trajectories, n_components, gamma):
         self.trajectories = trajectories
+        self.n_components = n_components
         self.gamma = gamma
         self.computeKPCA()
 
@@ -28,10 +29,10 @@ class KPCA(object):
         return (KPCA.RBF(t1[:, 0], t2[:, 0], gamma) + KPCA.RBF(t1[:, 1], t2[:, 1], gamma)) / 2
 
     def computeKPCA(self):
-        K = np.empty((len(trajectories), len(trajectories)))
-        for i in range(len(trajectories)):
-            for j in range(len(trajectories)):
-                K[i, j] = KPCA.MKR(trajectories[i], trajectories[j], self.gamma)
+        K = np.empty((len(self.trajectories), len(self.trajectories)))
+        for i in range(len(self.trajectories)):
+            for j in range(len(self.trajectories)):
+                K[i, j] = KPCA.MKR(self.trajectories[i], self.trajectories[j], self.gamma)
 
         # Centering the symmetric NxN kernel matrix.
         N = K.shape[0]
@@ -43,68 +44,76 @@ class KPCA(object):
         eigvals, eigvecs = eigh(K)
 
         # Obtaining the i eigenvectors that corresponds to the i highest eigenvalues.
-        alphas = np.column_stack((eigvecs[:, -i] for i in range(1, n_components + 1)))
-        lambdas = [eigvals[-i] for i in range(1, n_components + 1)]
+        alphas = np.column_stack((eigvecs[:, -i] for i in range(1, self.n_components + 1)))
+        lambdas = [eigvals[-i] for i in range(1, self.n_components + 1)]
 
         self.eigvecs = alphas
         self.eigvals = lambdas
         self.K = K
 
     def project(self, trajectory_new):
-        pair_dist = np.array([KPCA.MKR(trajectory_new, trajectory, self.gamma) for trajectory in trajectories])
+        pair_dist = np.array([KPCA.MKR(trajectory_new, trajectory, self.gamma) for trajectory in self.trajectories])
         return pair_dist.dot(self.eigvecs / self.eigvals)
+
+
+class Predictor(object):
+    def __init__(self, trajectories=None, labels=None, classifier=None, gamma=18):
+        if (trajectories is None and labels is not None) or \
+                (trajectories is not None and labels is None) or \
+                (trajectories is not None and labels is not None and (trajectories) != len(labels)):
+            raise ValueError("trajectories and labels must have the same size")
+
+        self.trajectories = [] if trajectories is None else trajectories
+        self.labels = [] if labels is None else labels
+        self.classifier = neighbors.KNeighborsClassifier(n_neighbors=3) if classifier is None else classifier
+        self.gamma = gamma
+        self.kpca = None
+
+    def addTrajectory(self, trajectory, label):
+        if trajectory is None or label is None:
+            raise ValueError("trajectory and label must not be None")
+
+        self.trajectories.append(trajectory)
+        self.labels.append(label)
+        self.kpca = None
+
+    def classify(self, trajectory):
+        if self.kpca is None:
+            unique_labels = set(self.labels)
+            self.kpca = KPCA(self.trajectories, len(unique_labels), self.gamma)
+            self.classifier.fit(self.kpca.eigvecs, self.labels)
+
+        projected = self.kpca.project(trajectory)
+        return self.classifier.predict(projected.reshape(1, -1))[0], projected
 
 
 if __name__ == '__main__':
     # Read in saved trajectories
-    trajectories_L = []
+    predictor = Predictor()
     for i in range(1, 21):
-        trajectories_L.append(np.load("data/L/{0}.npy".format(i)))
-    trajectories_O = []
+        predictor.addTrajectory(np.load("data/L/{0}.npy".format(i)), "L")
     for i in range(1, 21):
-        trajectories_O.append(np.load("data/O/{0}.npy".format(i)))
-    trajectories_R = []
+        predictor.addTrajectory(np.load("data/O/{0}.npy".format(i)), "O")
     for i in range(1, 21):
-        trajectories_R.append(np.load("data/R/{0}.npy".format(i)))
-    trajectories_S = []
+        predictor.addTrajectory(np.load("data/R/{0}.npy".format(i)), "R")
     for i in range(1, 21):
-        trajectories_S.append(np.load("data/S/{0}.npy".format(i)))
-    trajectories_W = []
+        predictor.addTrajectory(np.load("data/S/{0}.npy".format(i)), "S")
     for i in range(1, 21):
-        trajectories_W.append(np.load("data/W/{0}.npy".format(i)))
-
-    trajectories = []
-    trajectories.extend(trajectories_L)
-    trajectories.extend(trajectories_O)
-    trajectories.extend(trajectories_R)
-    trajectories.extend(trajectories_S)
-    trajectories.extend(trajectories_W)
-    n_components = 5
-
-    kpca = KPCA(trajectories, 18)
+        predictor.addTrajectory(np.load("data/W/{0}.npy".format(i)), "W")
 
     # Guess a new point
     test = np.load("out.npy")
-    test_projected = kpca.project(test)
-
-    classifier = neighbors.KNeighborsClassifier(n_neighbors=3)
-    classifier.fit(kpca.eigvecs,
-                   ["L"] * len(trajectories_L) + ["O"] * len(trajectories_O) + ["R"] * len(trajectories_R) +
-                   ["S"] * len(trajectories_S) + ["W"] * len(trajectories_W))
-    print(classifier.predict(test_projected.reshape(1, -1)))
+    prediction, projection = predictor.classify(test)
+    print(prediction)
 
     # Show plots
-    plt.imshow(kpca.K, interpolation="nearest")
+    plt.imshow(predictor.kpca.K, interpolation="nearest")
     plt.colorbar()
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection="3d")
-    ax.scatter(kpca.eigvecs[:, 0], kpca.eigvecs[:, 1], kpca.eigvecs[:, 2],
-               c=["r"] * len(trajectories_L) +
-                 ["b"] * len(trajectories_O) +
-                 ["k"] * len(trajectories_R) +
-                 ["g"] * len(trajectories_S) +
-                 ["c"] * len(trajectories_W),
+    ax.scatter(predictor.kpca.eigvecs[:, 0], predictor.kpca.eigvecs[:, 1], predictor.kpca.eigvecs[:, 2],
+               c=map(lambda l: {"L": "r", "O": "b", "R": "k", "S": "g", "W": "c"}[l], predictor.labels),
                marker="o")
-    ax.scatter(test_projected[0], test_projected[1], test_projected[2], c="y", marker="^")
+    ax.scatter(projection[0], projection[1], projection[2], c="y", marker="^")
     plt.show()
