b7d30c8ec786fff8b3c83a87a910ab2877a917ce
Ryan Zoeller <rtzoeller@rtzoeller.com>
2016-11-28 18:26:56 -0600

    Load training data from a parameter server

--- nodes/NormalizedTrajectoryMatcherNode.py
+++ nodes/NormalizedTrajectoryMatcherNode.py
@@ -3,16 +3,29 @@ from __future__ import division, print_function
 
 import rospy
 import numpy as np
+import os
 from std_msgs.msg import String
 from kpca_gesture_identifier.KPCA import Predictor
 from kpca_gesture_identifier.Point import Point
 from kpca_gesture_identifier.msg import Trajectory
 
 import rospkg
-rp = rospkg.RosPack()
-path = rp.get_path("kpca_gesture_identifier")
 
 publisher = rospy.Publisher("trajectory_shape", String, queue_size=5)
+
+# Read in saved trajectories
+rp = rospkg.RosPack()
+path = rp.get_path("kpca_gesture_identifier")
+gestures = rospy.get_param("gestures")
+predictor = Predictor()
+count = 0
+for gesture in gestures:
+    for directory, subdirectories, filePaths in os.walk(os.path.join(path, gestures[gesture])):
+        for filePath in filePaths:
+            fullPath = os.path.join(directory, filePath)
+            predictor.addTrajectory(np.load(fullPath), gesture)
+            count += 1
+rospy.logdebug("{0} samples loaded".format(count))
 
 
 def callback(trajectory):
@@ -20,23 +33,9 @@ def callback(trajectory):
     points = [Point(stampedPoint.point.x, stampedPoint.point.y, stampedPoint.header.stamp.to_sec())
               for stampedPoint in trajectory.points]
 
-    # Read in saved trajectories
-    predictor = Predictor()
-    for i in range(1, 21):
-        predictor.addTrajectory(np.load(path + "/" + "data/L/{0}.npy".format(i)), "L")
-    for i in range(1, 21):
-        predictor.addTrajectory(np.load(path + "/" + "data/O/{0}.npy".format(i)), "O")
-    for i in range(1, 21):
-        predictor.addTrajectory(np.load(path + "/" + "data/R/{0}.npy".format(i)), "R")
-    for i in range(1, 21):
-        predictor.addTrajectory(np.load(path + "/" + "data/S/{0}.npy".format(i)), "S")
-    for i in range(1, 21):
-        predictor.addTrajectory(np.load(path + "/" + "data/W/{0}.npy".format(i)), "W")
-
     # Guess a new point
     A = np.array([[p.x for p in points], [p.y for p in points]]).T
     prediction, projection = predictor.classify(A)
-    print(prediction)
     publisher.publish(prediction)
 
 
